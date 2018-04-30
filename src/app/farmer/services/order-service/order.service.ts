5a66674384f514501356edc645037f516ad6db5e
nishantgoyal880 <nishantgoyal880@gmail.com>
2018-04-30 10:55:55 +0530

    all the structure modifications has been completed and the project is working fine

--- src/app/farmer/services/order-service/order.service.ts
+++ src/app/farmer/services/order-service/order.service.ts
@@ -9,32 +9,31 @@ import { microServicesUrl } from '../../config/microServicesUrl.config';
 export class OrderService {
 
   constructor(private http : Http) { }
-  private headers = new Headers({ 'Content-Type': 'application/json'});
 
   //getting previous list of orders according to farmer id
   getPreviousOrderListFromFarmerId(farmerId) {
-    return this.http.get(microServicesUrl.orderPreviousStatus+farmerId)
+    return this.http.get(microServicesUrl.orderPreviousStatus+farmerId,this.authorization())
      .map(data => data.json(),
    (error: any)=>this.handleError(error));
    }
 
   //getting current list of orders according to farmer id
   getCurrentOrderListFromFarmerId(farmerId) {
-     return this.http.get(microServicesUrl.orderCurrentStatus+farmerId)
+     return this.http.get(microServicesUrl.orderCurrentStatus+farmerId,this.authorization())
       .map(data => data.json(),
     (error: any)=>this.handleError(error));
   }
 
   //Updating values of order details for delivery
   updateDeliveryDetails(updateValues) {
-      return this.http.put(microServicesUrl.orderUpdate,updateValues,{headers:this.headers})
+      return this.http.put(microServicesUrl.orderUpdate,updateValues,this.authorization())
        .map(data => data.json(),
      (error: any)=>this.handleError(error));
   }
 
   //Updating values of order details for decline reason
   updateDeclineReason(updatedReason) {
-       return this.http.put(microServicesUrl.orderUpdate,updatedReason,{headers:this.headers})
+       return this.http.put(microServicesUrl.orderUpdate,updatedReason,this.authorization())
         .map(data => data.json(),
     (error: any)=>this.handleError(error));
   }
@@ -44,4 +43,13 @@ export class OrderService {
      return Observable.throw(error.statusText);
   }
 
+  //code to send token in the header
+  private authorization() {
+    let token=localStorage.getItem("token");
+    if (token) {
+      let headers =new Headers();
+      headers.append('Authorization', token);
+      return new RequestOptions({ headers: headers });
+    }
+  }
 }
