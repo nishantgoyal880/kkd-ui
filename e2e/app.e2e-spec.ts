aae0fd6123377f6f8a13bb1f5b3db8deefa409a0
anu-bansal <anubansal0707@gmail.com>
2018-05-02 18:31:20 +0530

    e2e testing

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -8,6 +8,35 @@ describe('kkd-ui App', () => {
     page = new AppPage();
   });
 
+  it('should display Login SignUp button', () => {
+    page.navigateTo();
+    expect(page.getLoginSignUpButton()).toBeTruthy();
+  });
+
+  it('should display farmer Login button', () => {
+    page.navigateTo();
+    page.getLoginSignUpButton().click();
+    expect(page.getLoginFarmerButton()).toEqual('Login As Farmer');
+  });
+
+  it('should display farmer SignUp button', () => {
+    page.navigateTo();
+    page.getLoginSignUpButton().click();
+    expect(page.getSignUpFarmerButton()).toEqual('Sign Up As Farmer');
+  });
+
+  it('should display customer Login button', () => {
+    page.navigateTo();
+    page.getLoginSignUpButton().click();
+    expect(page.getLoginCustomerButton()).toEqual('Login As Customer');
+  });
+
+  it('should display customer SignUp button', () => {
+    page.navigateTo();
+    page.getLoginSignUpButton().click();
+    expect(page.getSignUpCustomerButton()).toEqual('Sign Up As Customer');
+  });
+
   it('should display Kisan Ki Dukan message', () => {
     page.navigateTo();
     expect(page.getBodyHeading()).toEqual('Kisan Ki Dukan');
@@ -20,6 +49,80 @@ describe('kkd-ui App', () => {
 
   it('should display farmer button', () => {
     page.navigateTo();
-    expect(page.getFarmerButton().getText()).toEqual('I\'m a farmer');
+    expect(page.getFarmerButton().getText()).toEqual('I\'m a Farmer');
   });
+
+  it('should display title after clicking farmer button', () => {
+    page.navigateTo();
+    page.getFarmerButton().click();
+    expect(page.getFarmerLoginTitle().getText()).toEqual('Login Farmer');
+  });
+
+  it('should display customer button', () => {
+    page.navigateTo();
+    expect(page.getCustomerButton().getText()).toEqual('I\'m a Customer');
+  });
+
+  it('should display title after clicking customer button', () => {
+    page.navigateTo();
+    page.getCustomerButton().click();
+    expect(page.getCustomerLoginTitle().getText()).toEqual('Login Customer');
+  });
+
+  it('should display home button', () => {
+    page.navigateTo();
+    expect(page.getHomeButton().getText()).toEqual('Home');
+  });
+
+  it('should display help button', () => {
+    page.navigateTo();
+    expect(page.getHelpButton().getText()).toEqual('Help');
+  });
+
+  it('should display help title', () => {
+    page.navigateTo();
+    page.getHelpButton().click();
+    expect(page.getHelpTitle().getText()).toEqual('Help');
+  });
+
+  it('should display help subTitle1', () => {
+    page.navigateTo();
+    page.getHelpButton().click();
+    expect(page.getHelpSubTitle1().getText()).toEqual('How to Register');
+  });
+
+  it('should display help subTitle2', () => {
+    page.navigateTo();
+    page.getHelpButton().click();
+    expect(page.getHelpSubTitle2().getText()).toEqual('Aadhaar Verification');
+  });
+
+  it('should display help subTitle3', () => {
+    page.navigateTo();
+    page.getHelpButton().click();
+    expect(page.getHelpSubTitle3().getText()).toEqual('Buying Products');
+  });
+
+  it('should display help subTitle4', () => {
+    page.navigateTo();
+    page.getHelpButton().click();
+    expect(page.getHelpSubTitle4().getText()).toEqual('Normal Order vs Bulk Order');
+  });
+
+  it('should display support button', () => {
+    page.navigateTo();
+    expect(page.getSupportButton().getText()).toEqual('Support');
+  });
+
+  it('should display support title', () => {
+    page.navigateTo();
+    page.getSupportButton().click();
+    expect(page.getSupportTitle().getText()).toEqual('Support');
+  });
+
+  it('should display product button', () => {
+    page.navigateTo();
+    expect(page.getProductButton().getText()).toEqual('Products');
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -2,8 +2,28 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/home');
   }
+
+  getLoginSignUpButton(){
+  return element(by.buttonText('LogIn & SignUp'));
+  }
+
+  getLoginFarmerButton(){
+    return element(by.css('app-header [routerLink="/farmer/login"]')).getText();
+    }
+  
+  getSignUpFarmerButton(){
+    return element(by.css('app-header [routerLink="/farmer/authenticationAndAuthorization"]')).getText();
+    }
+
+  getLoginCustomerButton(){
+    return element(by.css('app-header [routerLink="/customer/login"]')).getText();
+    }
+
+  getSignUpCustomerButton(){
+    return element(by.css('app-header [routerLink="/customer/register"]')).getText();
+    }
 
   getBodyHeading() {
     return element(by.css('app-body h1')).getText();
@@ -14,7 +34,58 @@ export class AppPage {
   }
 
   getFarmerButton() {
-    return element(by.css('[routerLink="/farmer/login"]')).getText();
+    return element(by.css('app-body [routerLink="/farmer/login"]'));
   }
 
+  getFarmerLoginTitle() {
+    return element(by.css('app-farmer-login h1'));
+  }
+
+  getCustomerButton() {
+    return element(by.css('app-body [routerLink="/customer/login"]'));
+  }
+
+  getCustomerLoginTitle() {
+    return element(by.css('app-customer-login h1'));
+  }
+
+  getHomeButton() {
+    return element(by.css('[routerLink="/home"]'));
+  }
+
+  getHelpButton() {
+    return element(by.css('[routerLink="/help"]'));
+  }
+
+  getHelpTitle() {
+    return element(by.css('app-help h1'));
+  }
+
+  getHelpSubTitle1() {
+    return element(by.css('[href="#card-element-114778"]'));
+  }
+
+  getHelpSubTitle2() {
+    return element(by.css('[href="#card-element-979782"]'));
+  }
+
+  getHelpSubTitle3() {
+    return element(by.css('[href="#card-element-111"]'));
+  }
+
+  getHelpSubTitle4() {
+    return element(by.css('[href="#card-element-222"]'));
+  }
+
+  getSupportButton() {
+    return element(by.css('[routerLink="/support"]'));
+  }
+
+  getSupportTitle() {
+    return element(by.css('app-support h1'));
+  }
+
+  getProductButton() {
+    return element(by.css('[routerLink="/productList"]'));
+  }
 }
